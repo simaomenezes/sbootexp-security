a375ab6eee0ebd4b9c6eac61c4b05b097ecd052e
Simão Neto <simaomenezesneto@gmail.com>
2025-01-15 15:21:48 +0000

    chore: Setting application, test controller and route

---
+++ src/test/java/io/github/simaomenezes/sbootexp_security/SbootexpSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.simaomenezes.sbootexp_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootexpSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
