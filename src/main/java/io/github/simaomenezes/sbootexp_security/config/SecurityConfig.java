1e8c6e839f4e06244bc4652a9672f978fbedb28d
Simão Neto <simaomenezesneto@gmail.com>
2025-01-15 19:04:16 +0000

    feat: Authentication with filter

--- src/main/java/io/github/simaomenezes/sbootexp_security/config/SecurityConfig.java
+++ src/main/java/io/github/simaomenezes/sbootexp_security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.github.simaomenezes.sbootexp_security.config;
 
+import io.github.simaomenezes.sbootexp_security.CustomFilter;
 import io.github.simaomenezes.sbootexp_security.PasswordMasterAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +14,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, PasswordMasterAuthenticationProvider passwordMasterAuthenticationProvider) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            PasswordMasterAuthenticationProvider passwordMasterAuthenticationProvider,
+            CustomFilter customFilter) throws Exception {
         return http
                 .authorizeHttpRequests(customizer ->{
                     customizer.requestMatchers("/public").permitAll();
@@ -28,6 +33,7 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .authenticationProvider(passwordMasterAuthenticationProvider)
+                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
